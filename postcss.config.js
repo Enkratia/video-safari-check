9610e137d6047d9b3e123f1095a123f1c30f2920
Enkratia <manofthetreasure@gmail.com>
2024-03-25 00:44:40 +0300

    m

---
+++ postcss.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins:
+    process.env.NODE_ENV === "production"
+      ? [
+          "postcss-flexbugs-fixes",
+          [
+            "postcss-preset-env",
+            {
+              autoprefixer: {
+                flexbox: "no-2009",
+              },
+              features: {
+                "custom-properties": false,
+              },
+            },
+          ],
+        ]
+      : [
+          // No transformations in development
+        ],
+};
